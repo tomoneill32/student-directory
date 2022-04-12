667f83990a5220e3b2865f04f86183451fc7ab32
Tom O'Neill <tomoneill32@gmail.com>
2022-04-12 10:57:30 +0100

    storing student information in directory as a hash

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 #putting students into an array
 students = [
-  "Dr. Hannibal Lecter", 
-  "Darth Vader",
-  "Nurse Ratched", 
-  "Michael Coreleone", 
-  "Alex DeLarge",
-  "The Wicked Witch of the West", 
-  "Terminator", 
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon", 
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Coreleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november},
 ]
 
 
@@ -19,9 +19,9 @@ def print_header
   puts "-----------"
 end
 
-def print(names)
-  names.each { |name| 
-    puts name}
+def print(students)
+  students.each { |student| 
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"}   
 end
 
 def print_footer(names)
