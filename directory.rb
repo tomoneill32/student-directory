483b59f00ae7fac22efa2c821889fb1c556d565a
Tom O'Neill <tomoneill32@gmail.com>
2022-04-12 17:33:13 +0100

    Interactive menu + can now write to a file

--- directory.rb
+++ directory.rb
@@ -1,20 +1,19 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
-  
   name = gets.chomp
   
   while !name.empty? do
     
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+  
     name = gets.chomp
   end
 
-  students
 end
 
 def print_header
@@ -22,16 +21,59 @@ def print_header
   puts "-----------"
 end
 
-def print(students)
-  students.each { |student| 
+def print_students_list
+  @students.each { |student| 
     puts "#{student[:name]} (#{student[:cohort]} cohort)"}   
 end
 
-def print_footer(names)
-  puts "Overall we have #{names.count} great students"
+def print_footer
+  puts "Overall we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I'm not sure what you mean, please try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu
