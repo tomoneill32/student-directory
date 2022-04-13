5668581e36dd3aa0a10a2e9bc49f7f775c6d6817
Tom O'Neill <tomoneill32@gmail.com>
2022-04-13 12:08:21 +0100

    Step 14 exercise 6

---
+++ exercises_2/exercise6.rb
@@ -0,0 +1,136 @@
+@students = []
+
+def add_students(name , cohort = :november)
+  @students << {name: name, cohort: :november}
+end
+  
+
+def input_students
+  input_message
+  name = STDIN.gets.chomp
+  
+  while !name.empty? do
+    
+    add_students(name)
+    puts "Now we have #{@students.count} students"
+
+    name = STDIN.gets.chomp
+  end
+
+end
+
+def input_message
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+end
+
+def print_header
+  puts "The students of Villains academy"
+  puts "-----------"
+end
+
+def print_students_list
+  @students.each { |student| 
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"}   
+end
+
+def print_footer
+  puts "Overall we have #{@students.count} great students"
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to a file"
+  puts "4. Load the list from a file"
+  puts "9. Exit"
+end
+
+def file_selection
+  puts "Please enter a filename"
+  STDIN.gets.chomp
+end
+
+def write_to_file(filename)
+  File.open(filename, "w") do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
+  end
+end
+
+def save_students(filename = "students.csv")  
+  if File.exist?(filename)
+    write_to_file(filename)
+  else
+    puts "That file does not exist"
+  end
+end
+
+def try_load_students
+  filename = ARGV.first
+  filename = "students.csv" if filename.nil?
+  if File.exist?(filename)
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
+def load_students(filename = "students.csv")
+  if File.exist?(filename)
+    File.open(filename,"r") do |file|
+      file.readlines.each do |line|
+        name, cohort = line.chomp.split(',')
+        add_students(name, cohort)
+      end
+    end
+  else
+    puts "Sorry, #{filename} does not exist"
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    success_message(selection)
+    input_students
+  when "2"
+    success_message(selection)
+    show_students
+  when "3"
+    success_message(selection)
+    save_students(file_selection)
+  when "4"
+    success_message(selection)
+    load_students(file_selection)
+  when "9"
+    exit
+  else
+    puts "I'm not sure what you mean, please try again"
+  end
+end
+
+def success_message(selection)
+  puts "You have selected option #{selection}"
+end
+
+def interactive_menu
+  try_load_students
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+interactive_menu
