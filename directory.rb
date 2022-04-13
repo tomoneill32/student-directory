47cd1cc60aff8fa3e304da31a3953b256747c1a7
Tom O'Neill <tomoneill32@gmail.com>
2022-04-13 09:41:03 +0100

    File now loads on startup if argument given

--- directory.rb
+++ directory.rb
@@ -4,14 +4,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  name = gets.chomp
+  name = STDIN.gets.chomp
   
   while !name.empty? do
     
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
   
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 
 end
@@ -49,8 +49,20 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv","r")
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exist?(filename)
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename,"r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -82,9 +94,10 @@ def process(selection)
 end
 
 def interactive_menu
+  try_load_students
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
